5e748c940b1d61b7e41ce091222113f72d0b6f90
MarkisRaresMarius <80154660+MarkisRaresMarius@users.noreply.github.com>
2021-03-06 23:00:38 +0200

    Add files via upload

--- Problema 5/src/app.js
+++ Problema 5/src/app.js
@@ -1,15 +1,65 @@
-document.getElementById('n').addEventListener('input ', inputSum);
+var opSign;
+document.getElementById("operationSign").innerHTML="";
+document.getElementById('n').addEventListener('input1', input);
 
-function inputSum() {
-  var inputNumber = parseInt(document.getElementById('n').value);
-  sum(inputNumber);
+document.getElementById('m').addEventListener('input2', input);
+
+document.getElementById('sum').addEventListener('click', changeSum);
+document.getElementById('rest').addEventListener('click', changeRest);
+document.getElementById('prod').addEventListener('click', changeProd);
+document.getElementById('cat').addEventListener('click', changeCat);
+
+function changeSum(){
+    opSign="+"
 }
 
-function sum(n) {
-  if (typeof n === 'undefined ') return 'n is undefined ';
+function changeRest(){
+    opSign="-"
+}
+
+function changeProd(){
+    opSign="*"
+}
+
+function changeCat(){
+    opSign="/"
+}
+
+function input() {
+  var inputNumber1 = parseInt(document.getElementById('n').value);
+  var inputNumber2 = parseInt(document.getElementById('m').value);
+  sum(inputNumber1,inputNumber2);
+}
+
+
+function sum(n,m) {
+  if (typeof n === 'undefined ') return 'first number is undefined ';
+  if (typeof m === 'undefined ') return 'second number is undefined ';
   var sum = 0;
-  for (var i = 1; i <= n; i++) {
-    sum += i;
-  }
+  sum += n+m;
   return sum;
+}
+
+function rest(n,m) {
+  if (typeof n === 'undefined ') return 'first number is undefined ';
+  if (typeof m === 'undefined ') return 'second number is undefined ';
+  var rest = 0;
+  rest += n-m;
+  return rest;
+}
+
+function prod(n,m) {
+  if (typeof n === 'undefined ') return 'first number is undefined ';
+  if (typeof m === 'undefined ') return 'second number is undefined ';
+  var prod = 0;
+  prod += n*m;
+  return prod;
+}
+
+function cat(n,m) {
+  if (typeof n === 'undefined ') return 'first number is undefined ';
+  if (typeof m === 'undefined ') return 'second number is undefined ';
+  var cat = 0;
+  cat += n/m;
+  return cat;
 }
