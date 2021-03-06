4639e83e032c1d899b42040d9de8b23960f937d3
MarkisRaresMarius <80154660+MarkisRaresMarius@users.noreply.github.com>
2021-03-06 23:00:11 +0200

    Add files via upload

---
+++ Problema 4/src/appTest.js
@@ -0,0 +1,6 @@
+console.log('Pentru 2: '+getFibonacci(2));
+console.log('Pentru 5: '+getFibonacci(5));
+console.log('Pentru a: '+getFibonacci('a'));
+console.log('Pentru 0: '+getFibonacci(0));
+console.log('Pentru 11: '+getFibonacci(11));
+
