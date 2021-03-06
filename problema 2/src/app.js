f60ceed923a4b41eebcc1294340a90dbaf9bd252
MarkisRaresMarius <80154660+MarkisRaresMarius@users.noreply.github.com>
2021-03-06 17:45:15 +0200

    problema 3

--- problema 2/src/app.js
+++ problema 2/src/app.js
@@ -1,15 +1,19 @@
-document.getElementById('n').addEventListener('input ', inputSum);
+var counter = 0;
 
-function inputSum() {
-  var inputNumber = parseInt(document.getElementById('n').value);
-  sum(inputNumber);
+function printValue(divId, value) {
+  document.getElementById(divId).innerHTML = value;
 }
+printValue('counter', 0);
 
-function sum(n) {
-  if (typeof n === 'undefined ') return 'n is undefined ';
-  var sum = 0;
-  for (var i = 1; i <= n; i++) {
-    sum += i;
-  }
-  return sum;
-}
+document.getElementById('inc').addEventListener('click', increment);
+document.getElementById('dec').addEventListener('click', decrement);
+function increment() {
+  if (counter+1 <= 10) {
+  counter++;
+  printValue('counter', counter);
+}}
+function decrement() {
+ if (counter-1 >= 0) {
+  counter--;
+  printValue('counter', counter);
+}}
