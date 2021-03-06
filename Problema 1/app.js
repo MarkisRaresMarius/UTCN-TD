b572c3abd8cc3727398544eed1ade8f120dabfe7
MarkisRaresMarius <80154660+MarkisRaresMarius@users.noreply.github.com>
2021-03-06 22:59:02 +0200

    Add files via upload

--- Problema 1/app.js
+++ Problema 1/app.js
@@ -1,2 +1,11 @@
+document.getElementById("message").HTML="hi stranger";
 console.log(" Welcome to data transmission ");
-document.getElementById("message").innerHTML="Message from JavaScript";
+var merge="test";
+$("#press").click(function() {
+    alert( "Handler for .click() called." );
+});
+
+//document.getElementById('press').addEventListener('click', showMessage);
+
+
+
